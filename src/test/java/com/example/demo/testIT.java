b7d505b911de652a4d7601b77b0e9459b69f5317
boiaryntseva <boiaryntseva@gmail.com>
2022-08-20 16:09:31 -0400

    Add book test is added

--- src/test/java/com/example/demo/testIT.java
+++ src/test/java/com/example/demo/testIT.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.controller.StorageBook;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class testIT {
 
@@ -32,5 +37,29 @@ public class testIT {
         System.out.println(response.getBody());
         System.out.println(response.getStatusCode());
         JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
+    @Test
+    public void addBookITTest(){
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+        HttpHeaders headers = new HttpHeaders(); //creates headers
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity <StorageBook>httpEntity =
+                new HttpEntity <StorageBook>(buildBook(), headers);// converts java object to json string (body), Request
+
+        ResponseEntity <String> responseEntity =
+                testRestTemplate.postForEntity("http://localhost:8080/addBook", httpEntity, String.class );
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals(buildBook().getId(),responseEntity.getHeaders().get("unique").get(0));
+    }
+    public StorageBook buildBook() {
+        StorageBook storageBook = new StorageBook();
+        storageBook.setBookName("Test Book");
+        storageBook.setId("unittest123");
+        storageBook.setAuthor("spring");
+        storageBook.setIsbn("unittest");
+        storageBook.setAisle(123);
+        return storageBook;
     }
 }
